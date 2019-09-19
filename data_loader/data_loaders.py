aae7e4bd079f72c0d8cb7be3fe703db6a1bc049d
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-09-19 15:57:42 +0300

    Data producer for skip-gram token model

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,46 @@
-from torchvision import datasets, transforms
-from base import BaseDataLoader
+import pandas as pd
+import torch
+import numpy as np
 
+class DataProducer:
+    def __init__(self, token_paths, n_tokens, token2id, token_counts, batch_size, num_neg_samples):
+        self.token_paths = token_paths
+        self.n_tokens = n_tokens
+        self.token2id = token2id
+        self.num_neg_samples = num_neg_samples
+        self.batch_size = batch_size
+        self.p_distr = self.get_distribution(token_counts)
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+    @staticmethod
+    def get_distribution(token_counts):
+        word_counts = torch.FloatTensor(token_counts)
+        distr = word_counts / word_counts.sum()
+        distr = distr.pow(3 / 4)
+        distr = distr / distr.sum()
+        return distr.numpy()
+
+    def neg_sample(self, batch_size):
+        return np.random.choice(self.n_tokens, size=(batch_size, self.num_neg_samples), p=self.p_distr)
+
+    def generate_sample(self, token_paths):
+        for token_path in token_paths:
+            token_counts = pd.read_csv(token_path)
+            token_indices = [self.token2id[token] for token in token_counts.token]
+            for center in token_indices:
+                for target in token_indices:
+                    if center != target:
+                        yield center, target
+
+    def get_batch(self, iterator, batch_size):
+        """ Group a numerical stream into batches and yield them as Numpy arrays. """
+        while True:
+            center_batch = np.zeros(batch_size, dtype=np.int32)
+            target_batch = np.zeros(batch_size, dtype=np.int32)
+            for index in range(batch_size):
+                center_batch[index], target_batch[index] = next(iterator)
+            yield center_batch, target_batch
+
+    def batch_iterator(self):
+        single_gen = self.generate_sample(self.token_paths)
+        batch_gen = self.get_batch(single_gen, batch_size=self.batch_size)
+        return batch_gen
